f4bed27dd388d7572c0d52b401db24f3753a81e1
Brice Vandeputte <brice.vandeputte@orange.com>
2024-03-26 14:31:38 +0100

    Fix #5 add ipinfo sample

---
+++ common/bash_environment.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# exit code list: https://ioflood.com/blog/bash-exit-codes/
+# BASH_EXIT_SUCCESS=0
+BASH_EXIT_ERROR=1
+# BASH_EXIT_COMMAND_MIS_USAGE=2
+# BASH_EXIT_COMMAND_CANNOT_EXECUTE=126
+# BASH_EXIT_COMMAND_NOT_FOUND=127
+
+
+load_local_dot_env_file() {
+  DEFAULT_LOCAL_DOT_ENV="./env"
+  LOCAL_DOT_ENV=${1:-${DEFAULT_LOCAL_DOT_ENV}}
+  if [ ! -f "${LOCAL_DOT_ENV}" ]; then
+    echo "$LOCAL_DOT_ENV does not exist."
+    exit ${BASH_EXIT_ERROR};
+  fi
+  # shellcheck source=.env
+  source "${LOCAL_DOT_ENV}" set
+  # echo "${LOCAL_DOT_ENV} loaded";
+}
+
+expect_variable_to_be_set() {
+  var_name=$1
+  # echo "expect ${var_name}"
+  if [ -z "${!var_name}" ]; then
+    echo "expect environment variable ${var_name} to be set"
+    exit ${BASH_EXIT_ERROR};
+  # else
+  #   echo "variable ${var_name} is set to ${!var_name}"
+  fi
+}
