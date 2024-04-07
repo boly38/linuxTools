5fef25a0b5c1f86acea2cd06d2475af263ad7067
Brice Vandeputte <brice.vandeputte@orange.com>
2024-04-07 15:37:14 +0200

    Fix #9 add shellcheck

--- threads/testThreads.sh
+++ threads/testThreads.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # setup JVM memory limits
-# export JAVA_OPTS="-Xms12029m -Xmx12029m -Xss228K"
-export JAVA_OPTS="-Xms22029m -Xmx22029m -XX:ThreadStackSize=228 -XX:VMThreadStackSize=228"
+java_args=("-Xms22029m" "-Xmx22029m" "-XX:ThreadStackSize=228" "-XX:VMThreadStackSize=228")
 
 # report current JVM info about threads
-java $JAVA_OPTS -XX:+PrintFlagsFinal -version | grep -iE 'HeapSize|PermSize|ThreadStackSize'
+java "${java_args[@]}" -XX:+PrintFlagsFinal -version | grep -iE 'HeapSize|PermSize|ThreadStackSize'
 
 # start test reusing JVM config from JAVA_OPTS
-MAX_THREAD_LIMIT=100000 java $JAVA_OPTS MaxThreadCounter
+MAX_THREAD_LIMIT=100000 java "${java_args[@]}" MaxThreadCounter
